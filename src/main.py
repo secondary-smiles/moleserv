b3b7303ff96a53698bc82d584cdae64e73c0f92b
Shav Kinderlehrer <shav@trinket.icu>
2023-11-03 23:38:23 -0400

    Implement basic TLS server

--- src/main.py
+++ src/main.py
@@ -1,5 +1,24 @@
+import socket
+
+from OpenSSL import SSL
+
 def main():
-    pass
+    ctx = SSL.Context(SSL.SSLv23_METHOD)
+    ctx.use_privatekey_file("key.pem")
+    ctx.use_certificate_file("cert.pem")
+
+    server = SSL.Connection(ctx, socket.socket(socket.AF_INET, socket.SOCK_STREAM))
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server.bind(("127.0.0.1", 2693))
+    server.listen(15)
+
+    while True:
+        conn, addr = server.accept()
+        print(f"New connection from '{addr}'")
+
+        conn.send("Hello, world!\n".encode())
+        conn.shutdown()
+        conn.close()
 
 if __name__ == "__main__":
     main()
