ab807ad5cd262b2fb667ade736f52447183b96a1
Shav Kinderlehrer <shav@trinket.icu>
2023-11-07 19:11:44 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
         name="moleserv",
-        version="0.1.0",
+        version="0.1.1",
         description="Molerat protocol server library.",
         url="https://git.trinket.icu/moleserv.git",
 
